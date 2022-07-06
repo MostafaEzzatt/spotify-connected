b1abf55b90317e1eaf9210c22969d858d2e6755b
Mostafa Ezzat <psmostafa@gmail.com>
2022-07-06 04:46:11 +0200

    remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
 import PrimaryButtonLink from "../components/PrimaryButtonLink";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
     return (
