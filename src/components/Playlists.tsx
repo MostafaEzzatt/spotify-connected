4c2bb0c90d2cdc51aaf351eea5244ee43f06d231
Mostafa Ezzat <psmostafa@gmail.com>
2022-07-10 22:01:03 +0200

    finish remove links from playlists and top artists (#1)

--- src/components/Playlists.tsx
+++ src/components/Playlists.tsx
@@ -17,30 +17,35 @@ const Playlists = (props: Props) => {
                         .slice(0, show || playLists.items.length)
                         .map((item) => {
                             return (
-                                <Link
-                                    href={`/playlists/${item.id}`}
+                                // <Link
+                                // href={`/playlists/${item.id}`}
+                                // key={item.id}
+                                // >
+                                // <a className="mx-auto w-list-item sm:w-full">
+                                <div
+                                    className="mx-auto w-list-item sm:w-full"
                                     key={item.id}
                                 >
-                                    <a className="mx-auto w-list-item sm:w-full">
-                                        <div className="flex h-full flex-col items-center justify-end bg-listBlock p-4 drop-shadow transition-shadow hover:drop-shadow-md">
-                                            <div className="mb-2 w-full">
-                                                <CustomeImage
-                                                    image={item?.images[0]?.url}
-                                                    alt={item.name}
-                                                    type={item.type}
-                                                />
+                                    <div className="flex h-full flex-col items-center justify-end bg-listBlock p-4 drop-shadow transition-shadow hover:drop-shadow-md">
+                                        <div className="mb-2 w-full">
+                                            <CustomeImage
+                                                image={item?.images[0]?.url}
+                                                alt={item.name}
+                                                type={item.type}
+                                            />
+                                        </div>
+                                        <div className="w-full max-w-full">
+                                            <div className="max-w-full truncate font-bold uppercase text-white">
+                                                {item.name || "No Name"}
                                             </div>
-                                            <div className="w-full max-w-full">
-                                                <div className="max-w-full truncate font-bold uppercase text-white">
-                                                    {item.name || "No Name"}
-                                                </div>
-                                                <div className="text-sm text-gray-400">
-                                                    {item.tracks.total} tracks
-                                                </div>
+                                            <div className="text-sm text-gray-400">
+                                                {item.tracks.total} tracks
                                             </div>
                                         </div>
-                                    </a>
-                                </Link>
+                                    </div>
+                                </div>
+                                // </a>
+                                // </Link>
                             );
                         })}
                 </div>
--- src/components/TopArtists.tsx
+++ src/components/TopArtists.tsx
@@ -23,30 +23,35 @@ const TopArtists = (props: Props) => {
                         .slice(0, show || artists.items.length)
                         .map((item) => {
                             return (
-                                <Link
-                                    href={`/top_artists/${item.id}`}
+                                // <Link
+                                //     href={`/top_artists/${item.id}`}
+                                //     key={item.id}
+                                // >
+                                //     <a className="mx-auto w-list-item sm:w-full">
+                                <div
+                                    className="mx-auto w-list-item sm:w-full"
                                     key={item.id}
                                 >
-                                    <a className="mx-auto w-list-item sm:w-full">
-                                        <div className="flex h-full w-list-item flex-col items-center justify-end bg-listBlock p-4 drop-shadow transition-shadow hover:drop-shadow-md sm:w-full">
-                                            <div className="mb-2 w-full">
-                                                <CustomeImage
-                                                    image={item?.images[0]?.url}
-                                                    alt={item.name}
-                                                    type={"user"}
-                                                />
+                                    <div className="flex h-full w-list-item flex-col items-center justify-end bg-listBlock p-4 drop-shadow transition-shadow hover:drop-shadow-md sm:w-full">
+                                        <div className="mb-2 w-full">
+                                            <CustomeImage
+                                                image={item?.images[0]?.url}
+                                                alt={item.name}
+                                                type={"user"}
+                                            />
+                                        </div>
+                                        <div className="w-full max-w-full">
+                                            <div className="truncate font-bold uppercase text-white">
+                                                {item.name || "No Name"}
                                             </div>
-                                            <div className="w-full max-w-full">
-                                                <div className="truncate font-bold uppercase text-white">
-                                                    {item.name || "No Name"}
-                                                </div>
-                                                <div className="text-sm text-gray-400">
-                                                    Artist
-                                                </div>
+                                            <div className="text-sm text-gray-400">
+                                                Artist
                                             </div>
                                         </div>
-                                    </a>
-                                </Link>
+                                    </div>
+                                </div>
+                                // </a>
+                                // </Link>
                             );
                         })}
                 </div>
