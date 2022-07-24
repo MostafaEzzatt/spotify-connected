6c28219e5493afb8f9b90ba1080a31285e535698
Mostafa Ezzat <psmostafa@gmail.com>
2022-07-24 16:44:52 +0200

    add an external link to top artists

--- src/components/TopArtists.tsx
+++ src/components/TopArtists.tsx
@@ -23,37 +23,42 @@ const TopArtists = (props: Props) => {
                         .slice(0, show || artists.items.length)
                         .map((item) => {
                             return (
-                                // <Link
-                                //     href={`/top_artists/${item.id}`}
-                                //     key={item.id}
-                                // >
-                                //     <a className="mx-auto w-list-item sm:w-full">
-                                <div
-                                    className="mx-auto w-list-item sm:w-full"
+                                <Link
+                                    href={item.external_urls.spotify}
                                     key={item.id}
                                 >
-                                    <div className="flex h-full w-list-item flex-col items-center justify-end bg-listBlock p-4 drop-shadow transition-shadow hover:drop-shadow-md sm:w-full">
-                                        <div className="mb-2 w-full">
-                                            <CustomeImage
-                                                image={item?.images[0]?.url}
-                                                alt={item.name}
-                                                type={"user"}
-                                                width={375}
-                                                height={375}
-                                            />
-                                        </div>
-                                        <div className="w-full max-w-full">
-                                            <div className="truncate font-bold uppercase text-white">
-                                                {item.name || "No Name"}
+                                    <a
+                                        className="mx-auto w-list-item sm:w-full"
+                                        target="_blank"
+                                    >
+                                        <div
+                                            className="mx-auto w-list-item sm:w-full"
+                                            key={item.id}
+                                        >
+                                            <div className="flex h-full w-list-item flex-col items-center justify-end bg-listBlock p-4 drop-shadow transition-shadow hover:drop-shadow-md sm:w-full">
+                                                <div className="mb-2 w-full">
+                                                    <CustomeImage
+                                                        image={
+                                                            item?.images[0]?.url
+                                                        }
+                                                        alt={item.name}
+                                                        type={"user"}
+                                                        width={375}
+                                                        height={375}
+                                                    />
+                                                </div>
+                                                <div className="w-full max-w-full">
+                                                    <div className="truncate font-bold uppercase text-white">
+                                                        {item.name || "No Name"}
+                                                    </div>
+                                                    <div className="text-sm text-gray-400">
+                                                        Artist
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div className="text-sm text-gray-400">
-                                                Artist
-                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                                // </a>
-                                // </Link>
+                                    </a>
+                                </Link>
                             );
                         })}
                 </div>
