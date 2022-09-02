fafb73374e9a614d2221fe44e991d778ec5acb6b
Mostafa Ezzat <psmostafa@gmail.com>
2022-09-02 10:20:50 +0200

    change cart item link based on item type

--- src/components/cards/CardItem.tsx
+++ src/components/cards/CardItem.tsx
@@ -6,7 +6,11 @@ import CustomeImage from "../CustomeImage";
 
 const CardItem = ({ item }: { item: playItemType | artistItemType }) => {
     return (
-        <Link href={`/playlists/${item.id}`}>
+        <Link
+            href={`/${item.type === "playlist" ? "playlists" : "artist"}/${
+                item.id
+            }`}
+        >
             <a className="mx-auto w-list-item sm:w-full">
                 <div className="flex h-full w-list-item flex-col items-center justify-end bg-listBlock p-4 drop-shadow transition-shadow hover:drop-shadow-md sm:w-full">
                     <div className="mb-2 w-full">
