965b292947ed373a4466ca3149f5a83a77d0bd37
Mostafa Ezzat <psmostafa@gmail.com>
2022-09-02 13:58:44 +0200

    finish display artist data and add placeholder for playlist name

--- src/pages/artist/[id].tsx
+++ src/pages/artist/[id].tsx
@@ -1,13 +1,11 @@
 import { GetServerSideProps } from "next";
-import { useInfiniteQuery, useQueries, useQuery } from "react-query";
+import Link from "next/link";
+import { useQuery } from "react-query";
+import CustomeImage from "../../components/CustomeImage";
 import LoadingFullScreen from "../../components/LoadingFullScreen";
-import ProfileHeader from "../../components/ProfileHeader";
 import withAuth from "../../components/protected/withAuth";
 import getRequests from "../../spotify/getRequest";
 import paths from "../../spotify/requestPaths";
-
-// types
-import type { item } from "../../types/spotifyArtistsResponse";
 
 const Artist = ({ id }: { id: string }) => {
     const { data, isLoading } = useQuery(["artist"], () => {
@@ -16,9 +14,69 @@ const Artist = ({ id }: { id: string }) => {
 
     if (isLoading) return <LoadingFullScreen />;
 
-    console.log(data);
+    return (
+        <div className="flex h-screen w-full flex-col items-center justify-center">
+            <div className="min-w-min">
+                <Link href={"/"}>
+                    <a className="font-bold text-highlight hover:text-highlight-press">
+                        {"<-"} Back Home
+                    </a>
+                </Link>
+                <div className="mx-auto max-w-max px-6 sm:px-0">
+                    {data.images.length > 0 && (
+                        <CustomeImage
+                            image={data?.images[0]?.url}
+                            alt={
+                                data.type == "user"
+                                    ? data.display_name
+                                    : data.name
+                            }
+                            type={data.type}
+                        />
+                    )}
+                </div>
+                <h1 className="break-words text-7xl font-black text-white sm:text-8xl">
+                    {data.name}
+                </h1>
 
-    return <></>;
+                <div className="my-3 mx-auto flex max-w-min items-center gap-2 text-center text-sm text-link">
+                    GENRES:
+                    {data.genres.map((g: string) => {
+                        return (
+                            <span
+                                key={g}
+                                className=" whitespace-nowrap rounded-full bg-highlight-press px-3 text-lg capitalize text-white"
+                            >
+                                {g.trim()}
+                            </span>
+                        );
+                    })}
+                </div>
+
+                <div className="flex justify-around">
+                    <p className="text-sm text-link">
+                        FOLLOWERS:{" "}
+                        <span className="text-xl text-highlight">
+                            {
+                                parseInt(data.followers.total)
+                                    .toLocaleString("en-US", {
+                                        minimumFractionDigits: 2,
+                                    })
+                                    .split(".")[0]
+                            }
+                        </span>
+                    </p>
+
+                    <p className="text-sm text-link">
+                        POPULARITY:{" "}
+                        <span className="text-xl text-highlight">
+                            {data.popularity}%
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
